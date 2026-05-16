af9a9704773a5fdb398c94515fc8a6cb31ff4055
elianisdev <elianismedina05@outlook.com>
2026-05-16 15:33:01 -0500

    feat: added detil screen for protections, structure and wiring

--- hooks/use-keyboard-offset.ts
+++ hooks/use-keyboard-offset.ts
@@ -9,33 +9,22 @@ export const useKeyboardOffset = () => {
     const onShow = (e: KeyboardEvent) => {
       keyboardOffset.value = withSpring(
         e.endCoordinates.height - (Platform.OS === "ios" ? 80 : 0),
-        {
-          damping: 20,
-          stiffness: 90,
-        }
+        { damping: 20, stiffness: 90 }
       );
     };
 
-    const showEvent = Platform.OS === "ios" ? "keyboardWillShow" : "keyboardDidShow";
-    const showSubscription = Keyboard.addListener(showEvent, onShow);
-
-    return () => {
-      showSubscription.remove();
-    };
-  }, [keyboardOffset]);
-
-  useEffect(() => {
     const onHide = () => {
-      keyboardOffset.value = withSpring(0, {
-        damping: 20,
-        stiffness: 90,
-      });
+      keyboardOffset.value = withSpring(0, { damping: 20, stiffness: 90 });
     };
 
+    const showEvent = Platform.OS === "ios" ? "keyboardWillShow" : "keyboardDidShow";
     const hideEvent = Platform.OS === "ios" ? "keyboardWillHide" : "keyboardDidHide";
+
+    const showSubscription = Keyboard.addListener(showEvent, onShow);
     const hideSubscription = Keyboard.addListener(hideEvent, onHide);
 
     return () => {
+      showSubscription.remove();
       hideSubscription.remove();
     };
   }, [keyboardOffset]);
